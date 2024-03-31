1c8e2a9fde47e4fc0712ac9f1cc9a6a465df666d
shafiq <makshafiq588@gmail.com>
2024-03-31 11:32:15 +0530

    vite.config file updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
+  // This changes the output dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
     outDir: "build",
